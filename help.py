c9b55e3fdea52813f138c5ccc41bab6d607bdab8
Vignesh2064 <96829523+Vignesh2064@users.noreply.github.com>
2024-06-25 10:42:32 +0530

    Update help.py

--- help.py
+++ help.py
@@ -7,7 +7,7 @@ def main():
     # Retrieve the token and username from environment variables
     hf_token = os.getenv('HF_TOKEN')
     username = os.getenv('USERNAME')
-    repo_name = "tt-oilwells-demo-model"
+    repo_name = "my-new-model"
     repo_id = f"{username}/{repo_name}"
 
     # Save the token
